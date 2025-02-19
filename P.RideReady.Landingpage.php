30272bca68bdb543804923a7cccec707c13eabe8
Adorjan Rieck <adorjan.rieck@icloud.com>
2025-02-19 13:51:38 +0100

    -LP Ready

--- P.RideReady.Landingpage.php
+++ P.RideReady.Landingpage.php
@@ -6,11 +6,6 @@
     <title>Homepage</title>
     <link rel="stylesheet" href="P.RideReady.css">
     <style>
-    .recommendations-container {
-        background-color: #F0F0F0;
-        padding: 20px;
-        text-align: center;
-    }
     /* ALL of the center alligned titles! */
     .landingpage-title {
         font-size: 24px;
@@ -25,6 +20,10 @@
         justify-content: center;
         gap: 20px;
         flex-wrap: wrap;
+        background-color: #F0F0F0;
+        padding: 20px;
+        text-align: center;
+        max-height: 340px;
     }
 
     .recommendation-card {
@@ -82,32 +81,32 @@
         
         <h2 class="landingpage-title">⚜️Willkommen bei Ride Ready⚜️</h2>
         <video autoplay muted playsinline class="teaser-titlepicture-image">
-            <source src="Titlepicture.mp4" type="video/mp4">
+            <source src="Images/Landingpage/Titlepicture.mp4" type="video/mp4">
         </video>
         <h2 class="landingpage-title">Mobilität, die zu Ihnen passt – Entdecken Sie unsere Top-Mietwagenangebote!</h2>
         <div class="teaser-grid">
             <div>
-                <img src="Mini Cabrio.LP.webp" alt="Cabrios" class="teaser-vehicle-image">
+                <img src="Images/Landingpage/Mini-Cabrio.LP.webp" alt="Cabrios" class="teaser-vehicle-image">
                 <p>Cabrios</p>
             </div>
             <div>
-                <img src="Mercedes GLS.LP.jpg" alt="SUVs" class="teaser-vehicle-image">
+                <img src="Images/Landingpage/Mercedes-GLS.LP.webp" alt="SUVs" class="teaser-vehicle-image">
                 <p>SUVs</p>
             </div>
             <div>
-                <img src="BMW M1.LP.jpeg" alt="Limousinen" class="teaser-vehicle-image">
+                <img src="Images/Landingpage/BMW-M1.LP.webp" alt="Limousinen" class="teaser-vehicle-image">
                 <p>Coupés</p>
             </div>
             <div>
-                <img src="VW Passat.LP.webp" alt="Combis" class="teaser-vehicle-image">
+                <img src="Images/Landingpage/VW-Passat.LP.webp" alt="Combis" class="teaser-vehicle-image">
                 <p>Combis</p>
             </div>
             <div>
-                <img src="VW Sharan.LP.webp" alt="Mehrsitzer" class="teaser-vehicle-image">
+                <img src="Images/Landingpage/VW-Sharan.LP.webp" alt="Mehrsitzer" class="teaser-vehicle-image">
                 <p>Mehrsitzer</p>
             </div>
             <div>
-                <img src="Mercedes E-Klasse.LP.webp" alt="Coupés" class="teaser-vehicle-image">
+                <img src="Images/Landingpage/Mercedes-E-Klasse.LP.webp" alt="Coupés" class="teaser-vehicle-image">
                 <p>Limousinen</p>
             </div>
         </div>
@@ -118,11 +117,11 @@
         <h2 class="landingpage-title">Unsere Top Empfehlungen:</h2>
         <div class="recommendations-wrapper">
             <div class="recommendation-card">
-                <img src="Mercedes SKlasse.LP.jpeg" alt="Mercedes-Benz S500L" class="recommendation-image">
+                <img src="Images/Landingpage/Mercedes-SKlasse.LP.webp" alt="Mercedes-Benz S500L" class="recommendation-image">
                 <div class="recommendation-text">Jetzt Neu bei uns: Der Mercedes-Benz S500L</div>
             </div>
             <div class="recommendation-card">
-                <img src="Mercedes SKlasse.LP.jpeg" alt="VW Polo" class="recommendation-image">
+                <img src="Images/Landingpage/VW-Golf.LP.webp" alt="VW Polo" class="recommendation-image">
                 <div class="recommendation-text">Besonders Günstig: Der VW Polo</div>
             </div>
         </div>
