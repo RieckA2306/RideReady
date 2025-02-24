70aebb3f06b4aa367c5aca8d50d9a2ce6dc78461
Johannes-Ripp <Johannes-ripp@outlook.de>
2025-02-24 11:05:43 +0100

    dbconfig signin

--- Signin.php
+++ Signin.php
@@ -6,8 +6,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastname = $_POST["lastname"];
     $email = $_POST["email"];
     $password = $_POST["password"];
-    $_SESSION["password"] = $password;
-
+    $_SESSION["password"] = $password;//muss raus
+    $password_hash=password_hash($password,PASSWORD_DEFAULT);
 }
-echo "$firstname"."<br>"."$lastname"."<br>"."$username"."<br>"."$email"."<br>"."$password";
-?>
+
+
+$stmt = $conn->prepare("INSERT INTO benutzer (username, passwort_hash) VALUES (?, ?)");
+// Beide Parameter in einem Aufruf binden
+$stmt->bind_param("ss", $username, $password_hash);
+$stmt->execute();
+
+$coded=password_hash($password,PASSWORD_DEFAULT);
+echo "$firstname"."<br>"."$lastname"."<br>"."$username"."<br>"."$email"."<br>"."$password"."<br>"."$coded";
+if(password_verify($password,$coded)){
+    echo "true";
+}
+ ?>
--- dbConfig.php
+++ dbConfig.php
@@ -5,7 +5,9 @@ $serverusername="root";
 // $serverpassword="123456";
 $dbname="benutzer";
 $conn = new mysqli("$servername", "$serverusername", "", "$dbname");
+// $conn = new PDO("mysql:host=$servername;dbname=$dbname", $serverusername,);
 
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if ($conn->connect_error) {
     die("Verbindung fehlgeschlagen: " . $conn->connect_error);
 }
