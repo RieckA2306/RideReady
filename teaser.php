4f17e7804ebeac1fe394062129707bea92b622a7
Johannes-Ripp <Johannes-ripp@outlook.de>
2025-02-26 19:30:09 +0100

    signin login

--- teaser.php
+++ teaser.php
@@ -6,7 +6,7 @@
     <body>
 <div class="card">
     <div class="cardbild" ></div>
-        <div class="cardtext">
+        <div <?php echo 'class="cardtext"'?>>
                 <p style=" font-size: 30px;"> <?php  echo $_SESSION['Vendor_Name'] ?></p>
                 <p style=" font-size: 30px;"> <?php  echo $_SESSION['carname'] ?></p>
                 <p style="padding:20px;text-align: right; font-size: 40px;"> <?php  echo $_SESSION['carprice']."€" ?></p>
