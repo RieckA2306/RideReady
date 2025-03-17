17059c4bf7e3be95faf2cde2fdca5d95b659b009
Dr3Josef <josef.dreischulte@outlook.de>
2025-03-17 13:24:42 +0100

    Productoverview Teaser Admin Kommentare

--- Productoverview_Teaser_Admin.php
+++ Productoverview_Teaser_Admin.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport">
-    <link rel="stylesheet" href="RideReady.css">
+    <link rel="stylesheet" href="RideReady.css?v=1.1">
 </head>
 <style>
 </style>
 <body>
-<a href="Productdetail_Admin.php?id=<?php echo urlencode($_SESSION['type_id']); ?>">
-    <div class="card">
+<div class="card">
+    <a href="Productdetail_Admin.php?id=<?php echo urlencode($_SESSION['type_id']); ?>">
         <div class="cardimage">
             <img src="Images/Cars/<?php echo htmlspecialchars($_SESSION['Img_File_Name']); ?>" alt="Car Image">
         </div>
@@ -19,19 +19,20 @@
             <div class="cardtext-Vendor_Name"> 
                 <?php echo htmlspecialchars($_SESSION['Vendor_Name'] . ' ' . $_SESSION['carname']); ?>
             </div>
+            <!-- Then the Name extension if there is any -->
             <div class="cardtext-Name_Extension">
                 <?php echo htmlspecialchars($_SESSION['Name_Extension']); ?>
             </div>
+            <!-- Then the Number of Cars at Ride Ready of that Type -->
             <div class="cardtext-available">
                 <?php echo "<p>Verfügbar: $availableCount</p>"; ?>
             </div>
-
+            <!-- And at Last the Price Per Day of teh Type of Car -->
             <div class="cardtext-price">
                 <?php echo number_format($_SESSION['carprice'], 2, ',', '.') . "€"; ?>
             </div>
-
         </div>
-        
-    </div> 
-</a>
+    </a>    
+</div> 
+
 </body>
