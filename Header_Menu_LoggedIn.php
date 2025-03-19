afacf947a81e5c4390719192e39e010de3f318bc
Adorjan Rieck <adorjan.rieck@icloud.com>
2025-03-19 13:50:37 +0100

    -Productdetail kleiner CSS Fix

--- Header_Menu_LoggedIn.php
+++ Header_Menu_LoggedIn.php
@@ -5,10 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <?php
- 
-
-
-    $username= $_SESSION["username"];
+ $username= $_SESSION["username"];
 ?>
 <body>
 <div class="menu" id="menu">
